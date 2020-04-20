027850e1fbe4b99c93bcf1b4c17cd768ff7ccbce
Azimbek <azimb3k@yandex.ru>
2020-04-20 19:20:33 +0600

    Sprint 3. hw02_community

--- posts/admin.py
+++ posts/admin.py
@@ -6,11 +6,13 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("pk", "text", "pub_date", "author")
     search_fields = ("text",)
     list_filter = ("pub_date",)
-    empty_value_display = '-пусто-'
+    empty_value_display = "-пусто-"
+
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "slug", "description")
     search_fields = ("description",)
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("group/<slug:slug>", views.group_posts)
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts)
 ]
